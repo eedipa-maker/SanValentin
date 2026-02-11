103f6972eea7bb9bfa547d4897e871212f4f1aa4
eedipa-maker <eedipa@hotmail.com>
2026-02-11 00:27:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const musica = document.getElementById("musica");
+let musicaIniciada = false;
+
+/* 🎵 reproducir música al PRIMER clic en cualquier parte */
+document.addEventListener("click", function(){
+    if(!musicaIniciada){
+        musica.play().catch(()=>{});
+        musicaIniciada = true;
+    }
+});
+
 function abrir(){
     const heart = document.getElementById("heart");
     const contenido = document.getElementById("contenido");
